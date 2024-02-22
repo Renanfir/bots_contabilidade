824594e4524c5dad0dcc979273682dba7530992f
Renanfir <renan260307@gmail.com>
2024-02-22 07:25:55 -0300

    bots

---
+++ sped.py
@@ -0,0 +1,9 @@
+import pyautogui as bot
+
+for i in range(25):
+    bot.doubleClick(1144, 389)
+    bot.sleep(0.3)
+    bot.click(1006, 526)
+    bot.click(998, 598)
+    bot.click(919, 712)
+    bot.click(1877, 387)
