04fc884a6e337b0b772afbbe83dc4fdf67c6b18e
Renanfir <renan260307@gmail.com>
2025-02-14 12:40:51 -0300

    exclusao

--- sped.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as bot
-
-for i in range(25):
-    bot.doubleClick(1144, 389)
-    bot.sleep(0.3)
-    bot.click(1006, 526)
-    bot.click(998, 598)
-    bot.click(919, 712)
-    bot.click(1877, 387)
